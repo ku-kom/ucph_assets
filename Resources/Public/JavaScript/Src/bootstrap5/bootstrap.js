969902fbb39439df529787f7a91a32e9ac94be7e
nanna-dk <nanna@e-nanna.dk>
2023-06-19 17:02:45 +0200

    Npm

---
+++ Resources/Public/JavaScript/Src/bootstrap5/bootstrap.js
@@ -0,0 +1,24 @@
+// Import the Bootstrap javascript libraries
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../../../node_modules/bootstrap/js/dist/alert.js";
+import "../../../../node_modules/bootstrap/js/dist/base-component.js";
+import "../../../../node_modules/bootstrap/js/dist/buttons.js";
+// import "../../../../node_modules/bootstrap/js/dist/carousel.js";
+import "../../../../node_modules/bootstrap/js/dist/collapse.js";
+import "../../../../node_modules/bootstrap/js/dist/dropdown.js";
+import "../../../../node_modules/bootstrap/js/dist/modal.js";
+import "../../../../node_modules/bootstrap/js/dist/offcanvas.js";
+import "../../../../node_modules/bootstrap/js/dist/popover.js";
+import "../../../../node_modules/bootstrap/js/dist/scrollspy.js";
+import "../../../../node_modules/bootstrap/js/dist/tab.js";
+import "../../../../node_modules/bootstrap/js/dist/toast.js";
+import "../../../../node_modules/bootstrap/js/dist/tooltip.js";
+
+//
+// Place any custom JS here
+//
+
+alert('test');
