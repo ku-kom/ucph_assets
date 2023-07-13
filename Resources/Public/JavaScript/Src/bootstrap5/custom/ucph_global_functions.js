e3a0085a00908c83bf3fd5ed451c9ab59b01aa72
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-13 13:27:58 +0200

    Added assets

--- Resources/Public/JavaScript/Src/bootstrap5/custom/ucph_global_functions.js
+++ Resources/Public/JavaScript/Src/bootstrap5/custom/ucph_global_functions.js
@@ -233,41 +233,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     animateMainmenu();
 
-    /**
-     * Link to open accordion
-     */
-    const slideToOpenAccordion = () => {
-        /**
-         * Return if Bootstrap collapse is not present
-         */
-        if (typeof bootstrap.Collapse !== 'function') {
-            return;
-        }
-
-        /**
-         * Check for accessibility settings
-         */
-        if (reduceMotion()) {
-            return;
-        }
-        if (window.location.hash !== '') {
-            // Open accordions or collapsible elements based on the hash in the url
-            let accordionExists = window.location.hash.indexOf('accordion-') >= 0 || window.location.hash.indexOf('collapse-') >= 0;
-            if (accordionExists) {
-                const accordionID = window.location.hash; // Variable includes hash (#).
-                // Open accordion if it exists
-                const collapsibleElement = document.querySelector(accordionID);
-                if (collapsibleElement) {
-                    new bootstrap.Collapse(collapsibleElement).show();
-                    // Scroll to accordion
-                    document.querySelector(accordionID).scrollIntoView({
-                        behavior: 'smooth'
-                    });
-                }
-            }
-        }
-    }
-    // slideToOpenAccordion();
+    
 
     window.addEventListener('scroll', () => {
         animatePageHeader();
@@ -277,14 +243,12 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     window.addEventListener('resize', debounce(function () {
-        //slideToOpenAccordion();
         getGlobalMenuHeight();
         animatePageHeader();
         animateMainmenu();
     }, 150));
 
     window.addEventListener('orientationchange', debounce(function () {
-        //slideToOpenAccordion();
         getGlobalMenuHeight();
         animatePageHeader();
         animateMainmenu();
